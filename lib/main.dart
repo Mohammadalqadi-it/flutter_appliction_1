eb749483a09bd90b75132e93b6412565ce2e52bf
Your Full Name <your.email@example.com>
2025-06-16 10:51:52 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/Strawberry_Pavlova_Recipe.dart';
-import 'package:flutter_application_1/widgets/app_bar_widget.dart';
-import 'package:flutter_application_1/widgets/column_widget.dart';
-import 'package:flutter_application_1/widgets/container_widget.dart';
-import 'package:flutter_application_1/widgets/icon_widget.dart';
+import 'package:flutter_application_1/widgets/custom_buttons.dart';
+import 'package:flutter_application_1/widgets/grid_view_widget.dart';
 import 'package:flutter_application_1/widgets/image_widget.dart';
-import 'package:flutter_application_1/widgets/row_widget.dart';
-import 'package:flutter_application_1/widgets/stack_widget.dart';
-import 'package:flutter_application_1/widgets/text_widgets.dart';
+import 'package:flutter_application_1/widgets/list_view_widget.dart';
+import 'package:flutter_application_1/widgets/padding_widget.dart';
+import 'package:flutter_application_1/widgets/page_view_widget.dart';
+import 'package:flutter_application_1/widgets/single_child_scroll_view_widget.dart';
+import 'package:flutter_application_1/widgets/text_button_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,7 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       // home: const RowWidget(),
-      home: const StrawberryPavlovaRecipe(),
+      home: const TextButtonWidget(),
     );
   }
 }
