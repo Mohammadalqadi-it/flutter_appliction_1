f1b64c17c425aa572f8a820da2af804ba5f1990b
Your Full Name <your.email@example.com>
2025-06-16 10:54:12 +0300

    image widget

--- lib/widgets/image_widget.dart
+++ lib/widgets/image_widget.dart
@@ -12,19 +12,16 @@ class ImageWidget extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
-      body: Column(
-        children: [
-          Image.network(
-            "https://i.guim.co.uk/img/media/9f1b249e73a0227f6ee0c9991f2dd72d13457e4d/0_128_2816_1690/master/2816.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=e927d7f65e482bbfa244c0916150c3d2",
-          ),
-          SizedBox(height: 10),
-          Image.asset(
-            AppImages.garden,
-            fit: BoxFit.fill,
-            height: 300,
-            width: 300,
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              "https://i.guim.co.uk/img/media/9f1b249e73a0227f6ee0c9991f2dd72d13457e4d/0_128_2816_1690/master/2816.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=e927d7f65e482bbfa244c0916150c3d2",
+            ),
+            SizedBox(height: 10),
+            Image.asset(AppImages.garden),
+          ],
+        ),
       ),
     );
   }
